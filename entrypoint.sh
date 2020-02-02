5fa9582a97bba3be000ece0cc3da2eb185462133
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-02-02 14:47:17 +0000

    Update entrypoint to use --work and --name

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,11 @@ REPO=$2
 TOKEN=$3
 NAME=$4
 
-echo ${NAME} | ./config.sh --url https://github.com/${OWNER}/${REPO} --token ${TOKEN} 
+./config.sh \
+    --url https://github.com/${OWNER}/${REPO} \
+    --token ${TOKEN} \
+    --name ${NAME} \
+    --work _work
 
 ./run.sh 
 
