92c0af22d721cbed5c694cc58bec1e3d9caf505c
Stephen OBrien <wayofthepie@users.noreply.github.com>
2020-02-08 15:27:30 +0000

    Use the checks api

--- orc.sh
+++ orc.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
-##
-# Detects new commits in a given repo by checking every minute
-# and storing a reference to the last seen commit in a file called
-# prev.
-##
 
 PAT=$1
 OWNER=$2
 REPO=$3
-
-cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-prev="${cur_dir}/prev"
 
 # make sure we have values for all our arguments
 [ -z ${PAT} ] || [ -z ${OWNER} ] || [ -z $REPO ] && {
@@ -18,29 +10,26 @@ prev="${cur_dir}/prev"
     exit 1
 }
 
-# make sure the prev file exists
-touch ${prev}
-
 # get the latest commit
 latest_commit=$(curl -s -H "authorization: token ${PAT}"  \
     https://api.github.com/repos/${OWNER}/${REPO}/commits |\
     jq -r .[0].sha)
 
-# read the last commit we saw
-prev_commit=$(cat ${prev})
+# for each check run requested for this commit, get the "status"
+# field and assign to the "check_status" variable 
+for check_status in $(curl -s \
+    -H "accept: application/vnd.github.antiope-preview+json" \
+    -H "authorization: token ${PAT}"\
+    https://api.github.com/repos/${OWNER}/${REPO}/commits/${latest_commit}/check-runs \
+    | jq -r '.check_runs[] | "\(.status)"'); do
 
-echo "Latest commit is ${latest_commit}"
-echo "Previous commit is ${prev_commit}"
-
-# compare the commits, running an action runner if they differ
-if [ "${latest_commit}" != "${prev_commit}" ]; then
-    echo "Detected new commit, starting runner"
-    docker run -d --rm actions-image \
-        ${OWNER} \
-        ${REPO} \
-        ${PAT} \
-        $(uuidgen)
-fi
-
-# update the previous commit store with the latest commit
-echo ${latest_commit} > ${prev}
+    # if "check_status" is queued launch an action runner
+    if [ "${check_status}" == "queued" ]; then
+        echo "Launching actions runner ..."
+        docker run -d --rm actions-image \
+            ${OWNER} \
+            ${REPO} \
+            ${PAT} \
+            $(uuidgen)
+    fi
+done
